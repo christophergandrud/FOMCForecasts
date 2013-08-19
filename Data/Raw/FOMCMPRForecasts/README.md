cd3a907f7a44379ebe9056f090dc892c3d2e3194
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-08-19 15:04:19 +0900

    FOMC MPR projections back to July 1979

--- Data/Raw/FOMCMPRForecasts/README.md
+++ Data/Raw/FOMCMPRForecasts/README.md
@@ -1,8 +1,10 @@
-## Monetary Policy Committee Predictions
+# Monetary Policy Committee Predictions Data set
 
 These data sets include predictions from the FOMC's Monetary Reports submitted to Congress biannually (February, July). The data was gathered from:
 
-> http://www.federalreserve.gov/monetarypolicy/mpr_default.htm
+> **Through February 1996:** <http://fraser.stlouisfed.org/publication/?pid=672&tid=87>
+
+> **From July 1996 onwards:** <http://www.federalreserve.gov/monetarypolicy/mpr_default.htm>
 
 The two CSV files contain forecasts concerning change in the quantities from fourth quarter to fourth quarter. The file `SameYear.csv` records predictions for the fourth quarter of the present year. `NextYear.csv` has predictions for the next year.
 
@@ -19,3 +21,9 @@ The variables other than `ForecastDate` use the following coding scheme:
 	- `Unem`: Civilian unemployment
 	
 - The final letter is either an `L` for the lower bound of the prediction or `H` for the higher bound.
+
+
+### Notes
+
+- The estimates for forecasts from the February 1988 were poorly scanned and visual approximations were made. See the file at: <http://fraser.stlouisfed.org/docs/historical/senate/cmp/1980s/CMP_100S_02241988.pdf>.
+
---
+++ SourceCode/Exploration/FOMC_MPR_PlotsExplore_2.R
@@ -0,0 +1,53 @@
+################
+# Graphs to explore FOMC monetary policy forecasts (2)
+# Christopher Gandrud
+# 19 August 2013
+################
+
+# Load packages
+library(ggplot2)
+library(gridExtra)
+
+# Create data sets from source
+source("/git_repositories/FOMCForecasts/SourceCode/DataPreparation/FOMCMPR_GreenBook_Combine.R")
+
+# Create inflation forecast graphs
+Plot1 <- ggplot(data = SameYearFeb, aes(x = year, y = ObsInfFebSame)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = SInflationL, ymax = SInflationH), alpha = 0.2) +
+        geom_ribbon(aes(ymin = SCInflationL, ymax = SCInflationH), alpha = 0.2) +
+        ylab("Inflation\n") + xlab("") + ggtitle("Forecast Made in February\n") + theme_bw()
+
+Plot2 <- ggplot(data = SameYearJul, aes(x = year, y = ObsInfJulySame)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = SInflationL, ymax = SInflationH), alpha = 0.2) +
+        geom_ribbon(aes(ymin = SCInflationL, ymax = SCInflationH), alpha = 0.2) +
+        ylab("") + xlab("") + ggtitle("Forecast Made in July\n") + theme_bw()
+
+# Create GDP forecast graphs
+Plot3 <- ggplot(data = SameYearFeb, aes(x = year, y = ObsGDP)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = SGDPL, ymax = SGDPH), alpha = 0.2) +
+        geom_ribbon(aes(ymin = SCGDPL, ymax = SCGDPH), alpha = 0.2) +
+        ylab("GDP Growth\n") + xlab("") + theme_bw()
+
+Plot4 <- ggplot(data = SameYearJul, aes(x = year, y = ObsGDP)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = SGDPL, ymax = SGDPH), alpha = 0.2) +
+        geom_ribbon(aes(ymin = SCGDPL, ymax = SCGDPH), alpha = 0.2) +
+        ylab("") + xlab("") + theme_bw()
+
+# Create Unemployment forecast graphs
+Plot5 <- ggplot(data = SameYearFeb, aes(x = year, y = ObsUnemp)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = SUnemL, ymax = SUnemH), alpha = 0.2) +
+        geom_ribbon(aes(ymin = SCUnemL, ymax = SCUnemH), alpha = 0.2) +
+        ylab("Unemployment\n") + xlab("") + theme_bw()
+
+Plot6 <- ggplot(data = SameYearJul, aes(x = year, y = ObsUnemp)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = SUnemL, ymax = SUnemH), alpha = 0.2) +
+        geom_ribbon(aes(ymin = SCUnemL, ymax = SCUnemH), alpha = 0.2) +
+        ylab("") + xlab("") + theme_bw()
+
+grid.arrange(Plot1, Plot2, Plot3, Plot4, Plot5, Plot6)
